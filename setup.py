de69d726169c45c176550100508945844ccdcb34
Chow Loong Jin <hyperair@debian.org>
2017-10-12 14:10:29 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='tlake8',
+    version='0.1',
+    description='flake8 for tab junkies',
+    author='Chow Loong Jin',
+    author_email='hyperair@debian.org',
+    url='https://www.github.com/hyperair/tlake8',
+    scripts=['tlake8'],
+    install_requires=['flake8']
+)
